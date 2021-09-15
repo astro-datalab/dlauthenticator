f066e7c26c9b5e042daa3ac1e7e000b143e4fe99
Data Lab User <datalab@noao.edu>
2021-09-14 17:45:06 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dlauthenticator',
-    version='0.1',
+    version='0.2',
     description='Data Lab Authenticator for JupyterHub',
     url='https://github.com/noaodatalab/dlauthenticator',
     author='Mike Fitzpatrick',
