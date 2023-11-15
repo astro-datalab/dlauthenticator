b8e2b96b463e8f6de596fe5ea6b711f056c28814
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2023-11-15 11:38:59 -0700

    corrected typo in GCDataLabAuthenticatorNoRedirect class name

--- dlauthenticator/__init__.py
+++ dlauthenticator/__init__.py
@@ -1,3 +1,5 @@
-from dlauthenticator.dlauthenticator import BaseDataLabAuthenticator, DataLabAuthenticator, GCDataLabAuthenticator, GCDataLabAuthenticatorNoRedirect
+from dlauthenticator.dlauthenticator import BaseDataLabAuthenticator, DataLabAuthenticator
+from dlauthenticator.dlauthenticator import GCDataLabAuthenticator, GCDataLabAuthenticatorNoRedirect
 
-__all__ = [BaseDataLabAuthenticator, DataLabAuthenticator, GCDataLabAuthenticator, GCDataLabAuthenticatorNoRedirect]
+__all__ = [BaseDataLabAuthenticator, DataLabAuthenticator,
+           GCDataLabAuthenticator, GCDataLabAuthenticatorNoRedirect]
